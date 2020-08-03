3ee18112f23b573a819c1dc653b19d8738c54bc5
Shahriar Khan <skhan@compass.edu.au>
2020-08-03 19:51:26 +1000

    fetchExchangeRate test added

---
+++ test/services/invoice.fetchExchangeRate.test.ts
@@ -0,0 +1,37 @@
+import * as path from "path";
+
+import axios from 'axios';
+import Invoice from '../../src/services/invoice';
+
+jest.mock('axios');
+const data = {
+    "rates": {
+        "USD": 0.6541135574,
+        "AUD": 0.9421205098
+    },
+    "base": "NZD",
+    "date": "2020-07-07"
+};
+const resp = {data};
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+const correctResult = { USD: 0.6541, AUD: 0.9421 };
+
+describe('Tests for the fetchExchangeRate method for the Invoice class', () => {
+
+  test('Should return an object containing the exchange rate',async () => {
+
+    //Arrange
+    mockedAxios.get.mockResolvedValue(resp);
+    const filePath = path.join(__dirname, "./01-input.txt");
+    const invoice = new Invoice(filePath);
+
+    //Act
+    const RESULT = await invoice.fetchExchangeRate();
+
+    //Assert
+    expect.assertions(1);
+    expect(RESULT).toEqual(correctResult);
+
+  });
+
+})
