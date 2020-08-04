e25a89f0ee91ada36c33511234ba702c79b81eeb
Shahriar Khan <skhan@compass.edu.au>
2020-08-04 12:17:38 +1000

    test for calculateInvoiceTotal

---
+++ test/services/invoice.calculateInvoiceTotal.test.ts
@@ -0,0 +1,24 @@
+import * as path from "path";
+
+import Invoice from '../../src/services/invoice';
+
+const lineTotal = [{"description":"Intel Core i9", "amount":1070.17},{"description":"ASUS ROG Strix", "amount":530.73}];
+const correctResult = 1600.9;
+
+describe('Tests for the calculateInvoiceTotal method for the Invoice class', () => {
+
+  test('Should return a number representing the invoice total',async () => {
+
+    //Arrange
+    const filePath = path.join(__dirname, "./01-input.txt");
+    const invoice = new Invoice(filePath);
+
+    //Act
+    const RESULT =  invoice.calculateInvoiceTotal(lineTotal);
+
+    //Assert
+    expect(RESULT).toEqual(correctResult);
+
+  });
+
+})
