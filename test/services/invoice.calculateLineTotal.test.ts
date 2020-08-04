39e4ab2170dd39a1a69500d76613cc791e9ac3a7
Shahriar Hasan Khan <shahriar27@hotmail.com>
2020-08-04 21:03:03 +1000

    test case modified

--- test/services/invoice.calculateLineTotal.test.ts
+++ test/services/invoice.calculateLineTotal.test.ts
@@ -1,27 +1,29 @@
 import * as path from "path";
 
-import Invoice from '../../src/services/invoice';
+import Invoice from "../../src/services/invoice";
 
-jest.mock('axios');
-const lineItems = [{"description":"Intel Core i9","currency":"USD","amount":700},{"description":"ASUS ROG Strix","currency":"AUD","amount":500}];
-const exchangeRates = { USD: 0.6541, AUD: 0.9421 };
-const correctResult = [{"description":"Intel Core i9", "amount":1070.17},{"description":"ASUS ROG Strix", "amount":530.73}];
+jest.mock("axios");
+const lineItems = [
+  { description: "Intel Core i9", currency: "USD", amount: 700 },
+  { description: "ASUS ROG Strix", currency: "AUD", amount: 500 },
+];
+const exchangeRates = { USD: 1.5288, AUD: 1.0614 };
+const correctResult = [
+  { description: "Intel Core i9", amount: 1070.16 },
+  { description: "ASUS ROG Strix", amount: 530.7 },
+];
 
-describe('Tests for the calculateLineTotal method for the Invoice class', () => {
-
-  test('Should return an array of object containing the line total',async () => {
-
+describe("Tests for the calculateLineTotal method for the Invoice class", () => {
+  test("Should return an array of object containing the line total", async () => {
     //Arrange
     const filePath = path.join(__dirname, "./01-input.txt");
     const invoice = new Invoice(filePath);
 
     //Act
-    const RESULT =  invoice.calculateLineTotal(lineItems, exchangeRates);
+    const RESULT = invoice.calculateLineTotal(lineItems, exchangeRates);
 
     //Assert
     expect.assertions(1);
     expect(RESULT).toEqual(correctResult);
-
   });
-
-})
+});
