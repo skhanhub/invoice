2a16dee9c83fa08c861c118309b9dc76b036be9d
Shahriar Hasan Khan <shahriar27@hotmail.com>
2020-08-04 21:38:40 +1000

    additional test added for failed cases

--- test/services/invoice.generateQuery.test.ts
+++ test/services/invoice.generateQuery.test.ts
@@ -1,10 +1,8 @@
-import Invoice from '../../src/services/invoice';
+import Invoice from "../../src/services/invoice";
 import * as path from "path";
 
-describe('Tests for the generateQuery method for the Invoice class', () => {
-
-  test('Should return a query string based on the inputfile content', () => {
-
+describe("Tests for the generateQuery method for the Invoice class", () => {
+  test("Should return a query string based on the inputfile content", () => {
     //Arrange
     const filePath = path.join(__dirname, "./01-input.txt");
     const invoice = new Invoice(filePath);
@@ -13,8 +11,16 @@ describe('Tests for the generateQuery method for the Invoice class', () => {
     const RESULT = invoice.generateQuery();
 
     //Assert
-    expect(RESULT).toEqual("https://api.exchangeratesapi.io/2020-07-07?base=NZD&symbols=USD,AUD");
-
+    expect(RESULT).toEqual(
+      "https://api.exchangeratesapi.io/2020-07-07?base=NZD&symbols=USD,AUD"
+    );
   });
 
-})
+  test("Should throw an error for invalid file path", () => {
+    //Assert
+    const filePath = path.join(__dirname, "./01-input.txtr");
+    expect(() => {
+      new Invoice(filePath);
+    }).toThrow();
+  });
+});
